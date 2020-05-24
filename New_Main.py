070a5220aa6e7d3c55de27f56304939deb3938a5
MrCosney <cosney@student.21-school.ru>
2020-05-24 05:02:59 +0300

    Small fix, replace cycles

--- New_Main.py
+++ New_Main.py
@@ -12,39 +12,38 @@ import sys
 def main():
     sims, data_sets, algs = setups()
     results = []
-    # choose data set
-    for sets in data_sets:
-        set_name = sets['type']
-        set_freq = sets['freq']
-        set_data = sets['data']
-        X = []
-        for wav in set_data:
-            X.append(load_wav(wav, set_freq))
-        X = normalization(np.array(X))
-        # choose mix type
-        for sim in sims:
-            mix_type = sim['mixture_type']
-            sim_name = sim['name']
+    # choose sim type
+    for sim in sims:
+        mix_type = sim['mixture_type']
+        sim_name = sim['name']
+        # choose data set
+        for sets in data_sets:
+            X = []
+            for wav in sets['data']:
+                X.append(load_wav(wav, sets['freq']))
+            X = normalization(np.array(X))
             # Run algorithms
             for alg in algs:
                 print("Running " + alg['name'] + " ...")
+                # choose mix type
                 if mix_type == 'linear':
-                    Mixd = mix(copy.deepcopy(X[:alg['microphones']]))
+                    alg['Mix'] = mix(copy.deepcopy(X[:alg['microphones']]))
                 elif mix_type == 'room':
-                    Mixd = makeroom(set_freq, copy.deepcopy(X[:alg['microphones']]))
+                    alg['Mix'] = makeroom(sets['freq'], copy.deepcopy(X[:alg['microphones']]))
                 else:
                     print("Error: Simulation is chosen wrong.")
                     sys.exit()
+                alg['Mix'] = normalization(alg['Mix'])
                 if 'options' in alg:
                     opt = alg['options']
                 else:
                     opt = None
-                alg['Unmix'], alg['state'] = alg['func'](Mixd, alg['state'], opt)
+                alg['Unmix'], alg['state'] = alg['func'](alg['Mix'], alg['state'], opt)
                 alg['Unmix'] = normalization(alg['Unmix'])
                 alg['Metrics' + sim_name] = evaluate(X, alg['Unmix'])
                 #results[alg['name']] = alg
-            res = {sim_name: algs}
-            results.append(res)
+            res = {sim_name + sets['type']: algs}
+            results.append(copy.deepcopy(res))
 
 
 def evaluate(X, S):
