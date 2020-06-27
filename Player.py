Add fixes with # of speakers, add Normalization and filtering

Player.py
@@ -38,26 +38,19 @@ def load_wav(filename, samplerate=44100):
 
 def play_and_record(X:np.array, data_set:dict, sim:dict):
     '''Play audio data on Speakers and Record via MiniDSP'''
+    import sys
     # TODO: Maybe rewrite with Multiprocessing Pool class, I tried but it wait the thread for some reason (Commented version on the bottom of this page)
-    vol_gain = 10000
+    vol_gain = 1000
     idx = speakers_device_idx()
 
     X = X * vol_gain
     recorder = Recorder(kwargs=({'fs': data_set['fs'],
                                  'chunk_size': sim['chunk_size'],
-                                 'audio_duration': data_set['audio_duration']}))
+                                 'audio_duration': data_set['audio_duration'],
+                                 'microphones': sim['microphones']}))
     rec = threading.Thread(target=recorder._record)
-    if X.shape[0] == 1:
-        s1 = threading.Thread(target=play, args=(X[0], idx[0]))
-        rec.start()
-        s1.start()
-    elif X.shape[0] == 2:
-        s1 = threading.Thread(target=play, args=(X[0], idx[0]))
-        s2 = threading.Thread(target=play, args=(X[1], idx[1]))
-        rec.start()
-        s1.start()
-        s2.start()
-    elif X.shape[0] == 3:
+    z = X.shape[0]
+    if len(idx) == 3:
         s1 = threading.Thread(target=play, args=(X[0], idx[0]))
         s2 = threading.Thread(target=play, args=(X[1], idx[1]))
         s3 = threading.Thread(target=play, args=(X[2], idx[2]))
@@ -66,6 +59,18 @@ def play_and_record(X:np.array, data_set:dict, sim:dict):
         s1.start()
         s2.start()
         s3.start()
+
+    elif len(idx) == 2:
+        s1 = threading.Thread(target=play, args=(X[0], idx[0]))
+        s2 = threading.Thread(target=play, args=(X[1], idx[1]))
+        rec.start()
+        s1.start()
+        s2.start()
+    else:
+        s1 = threading.Thread(target=play, args=(X[0], idx[0]))
+        rec.start()
+        s1.start()
+
     rec.join()
 
     return recorder._data

RecorderClass.py
@@ -10,8 +10,8 @@ class Recorder:
         self._rate = self.kwargs['fs'] if 'fs' in self.kwargs else 44100
         self._chunk_size = self.kwargs['chunk_size'] if 'chunk_size' in self.kwargs else 2048
         self._audio_duration = self.kwargs['audio_duration'] if 'audio_duration' in self.kwargs else 4
-        self._channels = 1      #TODO: Fix # of channels
-        self.device_idx = 1     #TODO: Fix to MiniDsp
+        self._channels = self.kwargs['microphones'] if 'microphones' in self.kwargs else 2
+        self.device_idx = self.device_idx()  # TODO: Fix to MiniDsp
         self.__stream = None
         self.__callbackFlag = None
         self._data = []
@@ -26,7 +26,7 @@ class Recorder:
                                       input_device_index=self.device_idx,
                                       stream_callback=self.__callback)
 
-        #TODO: Make normal stop that depends on audio duration
+        # TODO: Make normal stop that depends on audio duration
         while self.__stream.is_active():
             time.sleep(self._audio_duration)
             self._stop_record()
@@ -45,47 +45,23 @@ class Recorder:
         self._data.append(in_data)
         return np.zeros((0, 0)), self.__callbackFlag
 
-    #TODO:uncomment and fix in when get miniDSP
+    # TODO:uncomment and fix in when get miniDSP
 
-    # def device_idx(self):
-    #     """This method check if mic.array is connected or not and returns device index if it is connected"""
-    #
-    #     __device_idx = None
-    #     for i in range(self.__p.get_device_count()):
-    #         if "miniDSP" in self.__p.get_device_info_by_index(i)['name']:
-    #             __device_idx = i
-    #             return __device_idx
-    #
-    #     if __device_idx is None:
-    #             print('the MiniDsp is not connected! Using default input...')
-    #
-    #     return None
+    def device_idx(self):
+        """This method check if mic.array is connected or not and returns device index if it is connected"""
+        import sys
 
+        __device_idx = None
+        print('\033[96mMicrophone is:\033[0m')
+        for i in range(self.__p.get_device_count()):
+            if "miniDSP" in self.__p.get_device_info_by_index(i)['name']:
+                __device_idx = i
+                print("\t", self.__p.get_device_info_by_index(i)['name'])
+                return __device_idx
 
+        if __device_idx is None:
+            print('the MiniDsp is not connected! Using default input...')
+            sys.exit()
 
+        return None
 
-
-
-
-# def device_idx():
-#     import sys
-#
-#     p = pyaudio.PyAudio()
-#     device_idx = None
-#     print('\033[96mMicrophone is :\033[0m')
-#     for i in range(p.get_device_count()):
-#         if "miniDSP" in p.get_device_info_by_index(i)['name']:
-#             device_idx = i
-#             print("\t", p.get_device_info_by_index(device_idx)['name'])
-#             return device_idx
-#         #TODO:Small fix when don't have the miniDSP. Change the name below for correct work!
-#         elif "HD Webcam" in p.get_device_info_by_index(i)['name']:
-#             device_idx = i
-#             print("\t", p.get_device_info_by_index(device_idx)['name'])
-#             return device_idx
-#
-#     if device_idx == None:
-#         print("\033[31m {}".format('Error : No Microphone'))
-#         sys.exit()
-#
-#     return None

main_rtap_online.py
@@ -12,6 +12,7 @@ def main():
     for sim in sims:
         M = sim['microphones']
 
+        # 1. Load source signals
         for data_set in sim['data_sets']:
             X = []
             for wav in data_set['data']:
@@ -21,17 +22,31 @@ def main():
                     X.append(load_wav(wav, data_set['fs']))
                 else:
                     X.append(wav)
-
+            # 2. Normalize & format source signals
             X = form_source_matrix(X)
             X = normalization(np.array(X))
-            #calculate the max duration of audio for recording #Todo:fix for more fit
-            data_set['audio_duration'] = len(X[0]) / data_set['fs']
 
-            #Make the threads for Recorder and Player
+            # 3. Perform environment simulation (mix signals)
+            sim['options']['fs'] = data_set['fs']  # sampling frequency depends on the used data set
+            filtered, mixed, sim['mix_additional_outputs'] = mix(X, sim['mix_type'], sim['options'])
+
+            # 4. Produce audio data from speakers and record it by the board (Real Simulation) #Todo:fix for more fit
+            data_set['audio_duration'] = len(X[0]) / data_set['fs']
             rec_data = play_and_record(X, data_set, sim)
+
+
             # 4. Normalize filtered & mixed arrays
-            #TODO: Stopped here
-            a/5
+                # 4.1 Normalize Recorded Audio
+            for i in range(len(rec_data)):
+                rec_data[i] = normalization(rec_data[i])
+            sim['real_mixed'] = rec_data
+
+                # 4.2 Normalize Simulated Audio and filtered
+            sim['mixed'] = normalization(mixed)
+            for f in filtered:
+                filtered[...] = normalization(f)
+            sim['filtered'] = filtered
+
             # 5. Run algorithms
             for alg in sim['algs']:
                 if alg['name'].find('ILRMA') == 0 and data_set['type'] == 'Gen Signals':