Fixes - normalization, AIRES (batch), saving wav files for each dataset, automatically generating combinations of wav files, etc

Model.py
@@ -97,7 +97,7 @@ def mix_convolutive(S: np.array, sim: dict, data_set: dict) -> Tuple[np.ndarray,
     R = micro_locs[:, :M]
 
     room.add_microphone_array(pra.MicrophoneArray(R, room.fs))
-    #room.add_microphone_array(pra.Beamformer(R, room.fs))
+    # room.add_microphone_array(pra.Beamformer(R, room.fs))
     # Place the sources inside the room
     source_locs = np.array([
         [3., 3, 0.85],   # source 1
@@ -131,8 +131,8 @@ def mix_convolutive(S: np.array, sim: dict, data_set: dict) -> Tuple[np.ndarray,
 
     # Now mixed signals is just the sum
     mixed = np.sum(filtered, axis=0)
-    #room.plot(freq=[1000, 2000], img_order=0)
-    #plt.show()
+    # room.plot(freq=[1000, 2000], img_order=0)
+    # plt.show()
     return filtered, mixed, {'room_object': room}
 
 

Normalizer.py
@@ -22,11 +22,10 @@ def normalize_rowwise(S: np.ndarray) -> np.ndarray:
 
 def normalize(s: np.ndarray) -> np.ndarray:
     s = np.float64(s)
-    span = np.max(s) - np.min(s)
-    span = 1 if span == 0 else span  # safety check to avoid division by zero
-    minim = np.min(s)
-    s = ((s - minim) * 2) / span - 1
-    return s
+    # span = np.max(s) - np.min(s)
+    # span = 1 if span == 0 else span  # safety check to avoid division by zero
+    # s = ((s - np.min(s)) * 2) / span - 1
+    return s / np.max(np.abs(s))
 
 
 def normalize_old(S: np.ndarray) -> np.ndarray:

main.py
@@ -8,7 +8,6 @@ from mir_eval.separation import bss_eval_sources
 
 from datetime import datetime
 from typing import Tuple
-from pathlib import Path
 import numpy.linalg as nl
 
 
@@ -25,6 +24,7 @@ def main():
 
         # 1. Load source signals
         for data_set in sim['data_sets']:
+            print('\t\033[35mDataSet \'{}\' \033[0m'.format(data_set['name']))
             S = []
             for si, wav in enumerate(data_set['data']):
                 if si >= sim['sources']:
@@ -38,7 +38,7 @@ def main():
             S = normalize_rowwise(np.array(S))
             data_set['audio_duration'] = round(S.shape[1] / data_set['fs'], 1)
             # 3. Perform environment simulation (mix signals)
-            print('\033[35mMixing signals...\033[0m')
+            print('\t\t\033[35mMixing signals...\033[0m')
             filtered, mixed, sim = mix(S, sim, data_set)
 
             # 4. Normalize filtered & mixed arrays
@@ -48,17 +48,20 @@ def main():
                     f[i] = normalize(f[i])
             sim['filtered'] = filtered
             sim['mixed'] = mixed
+
             # 4.1. Save filtered & mixed plots
-            #plot_filtered(filtered, dir_sim_filtered)
-            #plot_mixed(mixed, dir_sim_mixed)
+            pr = "{}_".format(data_set['name'])
+            plot_original(S, dir_sim_mixed, pr, S.shape[1])
+            plot_filtered(filtered, dir_sim_filtered, pr, S.shape[1])
+            plot_mixed(mixed, dir_sim_mixed, pr, S.shape[1])
 
             # 4.2. Save filtered & mixed to wav
             for file_name, f in zip(data_set['file_names'], filtered):
                 for mi, m in enumerate(f):
-                    write("{}/{}_mic_{}.wav".format(dir_sim_filtered, Path(file_name).stem, mi), data_set['fs'], np.float32(m))
+                    write("{}/{}_{}_mic_{}.wav".format(dir_sim_filtered, data_set['name'], file_name, mi), data_set['fs'], np.float32(m))
 
             for mi, m in enumerate(mixed):
-                write("{}/mic_{}.wav".format(dir_sim_mixed, mi), data_set['fs'], np.float32(m))
+                write("{}/{}_mic_{}.wav".format(dir_sim_mixed, data_set['name'], mi), data_set['fs'], np.float32(m))
 
             # 4.3. Create list of chunks (online version only)
             if sim['run_type'] == 'online':
@@ -70,14 +73,14 @@ def main():
             SIR_temp = []
             SAR_temp = []
             # 5. Run algorithms
-            print('\033[35mSeparating...\033[0m')
+            print('\t\t\033[35mSeparating...\033[0m')
             for alg in sim['algs']:
 
-                if alg['name'].find('ILRMA') == 0 and data_set['type'] == 'Gen Signals':
+                if alg['name'].find('ILRMA') == 0 and data_set['name'] == 'Gen Signals':
                     print('Warning: artificially generated signals are not used with ILRMA, skipping...')
                     continue
 
-                print("\tSeparation by {} in simulation {} with chunk_size={} ..."
+                print("\t\t\tSeparation by {} in simulation {} with chunk_size={} ..."
                       .format(alg['name'], sim['name'], sim['chunk_size'] if 'chunk_size' in sim else '-'))
 
                 # 5.1 Run given algorithm (online or batch)
@@ -100,14 +103,13 @@ def main():
                 # 5.2 Save data to wav files
                 dir_alg = alg_create_folders(alg, dir_sim_unmixed)
                 for file_name, s in zip(data_set['file_names'], unmixed):
-                    write("{}/{}.wav".format(dir_alg, Path(file_name).stem), data_set['fs'], np.float32(s))
+                    write("{}/{}_{}.wav".format(dir_alg, data_set['name'], file_name), data_set['fs'], np.float32(s))
 
                 # 5.3 Compute metrics
-                alg['metrics'] = {data_set['type']: evaluate(S, sim['filtered'], unmixed)}
-                SDR_temp.append(alg['metrics']['Voice']['SDR'])
-                SIR_temp.append(alg['metrics']['Voice']['SIR'])
-                SAR_temp.append(alg['metrics']['Voice']['SAR'])
-
+                alg['metrics'] = {data_set['name']: evaluate(S, sim['filtered'], unmixed)}
+                SDR_temp.append(alg['metrics'][data_set['name']]['SDR'])
+                SIR_temp.append(alg['metrics'][data_set['name']]['SIR'])
+                SAR_temp.append(alg['metrics'][data_set['name']]['SAR'])
 
             # delete temporary "mixed" array form dict
             del sim['mixed']
@@ -115,7 +117,7 @@ def main():
             SARR.append(SAR_temp)
             SIRR.append(SIR_temp)
             # Create plots for this sim
-            #plot_sim_data_set_metrics(sim, data_set, dir_sim_plots)
+            # plot_sim_data_set_metrics(sim, data_set, dir_sim_plots)
 
         plot_boxes(SDRR, SARR, SIRR, sim['name'], dir_sim_box_plots)
         print('\033[35mSimulation \'{}\' finished\033[0m'.format(sim['name']))
@@ -124,7 +126,7 @@ def main():
     # Collect all metrics into new dictionary, display in in console with correct view and plot the results in folder
     rew_sims = rework_dict(sims)
     print_results(rew_sims)
-    #plot_metrics(rew_sims, dir_plots)
+    # plot_metrics(rew_sims, dir_plots)
     print('\033[35mAll done.\033[0m')
     print(SDRR)
 
@@ -132,12 +134,13 @@ def main():
 def evaluate(original: np.ndarray, filtered: np.ndarray, unmixed: np.ndarray) -> dict:
     ref = np.moveaxis(filtered, 1, 2)
     Ns = np.minimum(unmixed.shape[1], ref.shape[1])
-    #Sn = np.minimum(unmixed.shape[0], ref.shape[0])
-    #SDR, SIR, SAR, P = bss_eval_sources(unmixed[:, :Ns], ref[:, :Ns, 0])
+    # Sn = np.minimum(unmixed.shape[0], ref.shape[0])
     SDR, SIR, SAR, P = bss_eval_sources(ref[:, :Ns, 0], unmixed[:, :Ns])
-    # TODO: RMSE was removed because of Singular Matrix error, uncomment for check
     # return {'SDR': SDR, 'SIR': SIR, 'SAR': SAR, 'P': P, 'RMSE': rmse(original, unmixed)}
-    return {'SDR': np.round(np.mean(SDR), 2), 'SIR': np.round(np.mean(SIR), 2), 'SAR': np.round(np.mean(SAR), 2), 'P': P}
+    return {'SDR': np.round(np.mean(SDR), 2),
+            'SIR': np.round(np.mean(SIR), 2),
+            'SAR': np.round(np.mean(SAR), 2),
+            'P': P}
 
 
 def create_folders() -> Tuple[str, str]:

plots.py
@@ -3,7 +3,22 @@ import numpy as np
 import os
 
 
-def plot_filtered(filtered: np.array, dir_plots: str, samples: int = 20000):
+def plot_original(S: np.array, dir_plots: str, name_prefix: str = "", samples: int = 20000):
+    """Plot original audio data"""
+    figure_size = (15, 7)
+
+    fig, ax = plt.subplots(S.shape[0], sharex='col', sharey='row', figsize=figure_size)
+    fig.suptitle("Original", fontsize=18, fontweight="bold")
+    plt.grid()
+    for si, s in enumerate(S):
+        ax[si].plot(s[:samples])
+        ax[si].grid()
+        ax[si].set_title("Source {}".format(si+1), fontsize=18, fontweight="bold")
+    plt.savefig("{}/{}original.pdf".format(dir_plots, name_prefix))
+    # plt.show()
+
+
+def plot_filtered(filtered: np.array, dir_plots: str, name_prefix: str = "", samples: int = 20000):
     """Plot filtered audio data"""
     figure_size = (15, 7)
 
@@ -18,14 +33,13 @@ def plot_filtered(filtered: np.array, dir_plots: str, samples: int = 20000):
             ax[fi, mi].set_title("Source {}, microphone {}".format(fi+1, mi+1), fontsize=18, fontweight="bold")
 
     plt.grid()
-    plt.savefig("{}/filtered.pdf".format(dir_plots))
+    plt.savefig("{}/{}filtered.pdf".format(dir_plots, name_prefix))
     # plt.show()
 
 
-def plot_mixed(mixed: np.array, dir_plots: str, samples: int = 20000):
+def plot_mixed(mixed: np.array, dir_plots: str, name_prefix: str = "", samples: int = 20000):
     """Plot filtered and audio data"""
     figure_size = (15, 7)
-    samples = 20000
 
     fig, ax = plt.subplots(mixed.shape[0], sharex='col', sharey='row', figsize=figure_size)
     fig.suptitle("Mixed", fontsize=18, fontweight="bold")
@@ -34,7 +48,7 @@ def plot_mixed(mixed: np.array, dir_plots: str, samples: int = 20000):
         ax[mi].plot(m[:samples])
         ax[mi].grid()
         ax[mi].set_title("Microphone {}".format(mi+1), fontsize=18, fontweight="bold")
-    plt.savefig("{}/mixed.pdf".format(dir_plots))
+    plt.savefig("{}/{}mixed.pdf".format(dir_plots, name_prefix))
     # plt.show()
 
 

setups.py
@@ -1,4 +1,6 @@
 from algs.Algorithms import *
+from itertools import combinations
+from pathlib import Path
 
 
 def setups():
@@ -8,152 +10,18 @@ def setups():
     np.random.seed(0)
     n_samples = 20000
     time = np.linspace(0, 8, n_samples)
+    wav_folder = "Audio/Original"
+    # files = ["1.wav", "2.wav", "3.wav", "4.wav", "5.wav", "6.wav", "7.wav", "8.wav", "9.wav", "10.wav"]
+    files = ["1.wav", "2.wav", "3.wav"]
+    # files = ["Man.wav", "Woman.wav", "Announcer.wav"]
     data_sets = [
         {
-            'type': 'Voice',
+            'name': "".join([Path(f).stem for f in fs]),
             'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/2.wav"],
-            'file_names': ["1.wav", "2.wav"]
-         },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/3.wav"],
-            'file_names': ["1.wav", "3.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/4.wav"],
-            'file_names': ["1.wav", "4.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/5.wav"],
-            'file_names': ["1.wav", "5.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/6.wav"],
-            'file_names': ["1.wav", "6.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/7.wav"],
-            'file_names': ["1.wav", "7.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/8.wav"],
-            'file_names': ["1.wav", "8.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/9.wav"],
-            'file_names': ["1.wav", "9.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/1.wav", "Audio/Original/10.wav"],
-            'file_names': ["1.wav", "10.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/3.wav"],
-            'file_names': ["2.wav", "3.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/4.wav"],
-            'file_names': ["2.wav", "4.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/5.wav"],
-            'file_names': ["1.wav", "5.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/6.wav"],
-            'file_names': ["2.wav", "6.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/7.wav"],
-            'file_names': ["2.wav", "7.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/8.wav"],
-            'file_names': ["1.wav", "10.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/9.wav"],
-            'file_names': ["1.wav", "10.wav"]
-        },
-
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/2.wav", "Audio/Original/10.wav"],
-            'file_names': ["1.wav", "10.wav"]
-        },
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/3.wav", "Audio/Original/4.wav"],
-            'file_names': ["3.wav", "4.wav"]
-        },
-
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/3.wav", "Audio/Original/5.wav"],
-            'file_names': ["3.wav", "5.wav"]
-        },
-
-        {
-            'type': 'Voice',
-            'fs': 16000,
-            # 'data': ["Audio/Original/Man.wav", "Audio/Original/Woman.wav", "Audio/Original/Announcer.wav"],
-            'data': ["Audio/Original/3.wav", "Audio/Original/6.wav"],
-            'file_names': ["3.wav", "6.wav"]
-        }
+            'data': ['{}/{}'.format(wav_folder, f) for f in fs],
+            'file_names': [Path(f).stem for f in fs]
+        } for fs in combinations(files, 2)
     ]
-
     # Convolutive, batch, for 2 microphones
     conv_batch_2 = {
         'name': 'Convolutive_batch_2',
@@ -170,20 +38,19 @@ def setups():
             },
             {
                  'name': 'AUXIVA_10', 'func': auxiva, 'state': {}, 'options': {'stft_size': 2048, 'iter': 10}
-             },
+            },
             {
                  'name': 'AUXIVA_50', 'func': auxiva, 'state': {}, 'options': {'stft_size': 2048, 'iter': 50}
             },
             {
                 'name': 'ILRMA', 'func': ILRMA, 'state': {}, 'options': {'stft_size': 2048}
-             },
+            },
             {
                  'name': 'AIRES (rtap-opt)', 'func': AIRES_rtap, 'state': {}, 'options': {'type': 'opt'}
-             }
-            ,
+            },
             {
                 'name': 'AIRES (batch)', 'func': AIRES_batch, 'state': {}, 'options': {}
-         }
+            }
         ]
     }
 